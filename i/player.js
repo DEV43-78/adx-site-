67558dc5ca949b201c52626c0e17fa9fe1b74ab1
DEV43-78 <85327312+DEV43-78@users.noreply.github.com>
2025-07-04 19:04:53 +0530

    Update player.js

--- i/player.js
+++ i/player.js
@@ -56,7 +56,7 @@ if (!videoID) {
 
   playerBox.addEventListener('click', () => {
     if (state === 0) {
-      window.open("https://obqj2.com/4/9343270", "_blank"); // Open ad in new tab
+      window.open(" https://obqj2.com/4/9527084", "_blank"); // Open ad in new tab
       state = 1;
       return;
     }
